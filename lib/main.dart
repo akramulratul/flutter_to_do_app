536300fcfac9a384a6940a241e537ae1c0813c5b
akramul <akramulratul@gmail.com>
2022-04-10 17:26:10 +0600

    notification settings

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_app/ui/home_page.dart';
+import 'package:flutter_to_do_app/ui/theme.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeMode.dark,
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/ui/home_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../services/notification_services.dart';
+import '../services/theme_services.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var notifyHelper;
+  @override
+  void initState() {
+    //TOFO: implement initState
+    super.initState();
+    notifyHelper = NotifyHelper();
+    notifyHelper.requestIOSPermissions();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          Text(
+            "Theme Data",
+            style: TextStyle(fontSize: 30),
+          )
+        ],
+      ),
+    );
+  }
+
+  _appBar() {
+    return AppBar(
+      leading: GestureDetector(
+        onTap: () {
+          ThemeService().switchTheme();
+          notifyHelper.initializeNotification();
+          notifyHelper.displayNotification(
+              title: "Theme Changed",
+              body: Get.isDarkMode
+                  ? "Activated Light Theme"
+                  : "Activated Dark Theme");
+
+          notifyHelper.scheduledNotification();
+        },
+        child: Icon(
+          Icons.nightlight_round,
+          size: 20,
+        ),
+      ),
+      actions: [
+        Icon(
+          Icons.person,
+          size: 20,
+        ),
+        SizedBox(
+          width: 20,
+        ),
+      ],
+    );
+  }
+}
